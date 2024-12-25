826cf2e6547f2f32bba991d1258796978df0bf1e
IT21314742 <it21314742@my.sliit.lk>
2024-12-25 11:25:54 +0530

    Update
    Update

--- machine.py
+++ machine.py
@@ -24,7 +24,7 @@ def get_payout(row, bet):
             return bet * 5
         elif row[0] == '🔔':
             return bet * 10
-        elif row[0]
+        elif row[0] ==
 
 def main():
     balance = 100
